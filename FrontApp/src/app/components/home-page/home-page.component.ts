54c184f24bb7ef8ffd4dd5e598bdabddb54ae71a
BernardoZanette <bernardo7206@gmail.com>
2024-01-19 21:00:57 -0300

    adding background image in home page

--- FrontApp/src/app/components/home-page/home-page.component.ts
+++ FrontApp/src/app/components/home-page/home-page.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
-
+  image = "assets/home-page/home-back-ground.jpg"
 }
