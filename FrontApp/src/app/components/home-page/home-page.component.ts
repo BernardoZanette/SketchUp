f2967b38f02ea0b867634a1a5c2c84a46ab34093
BernardoZanette <bernardo7206@gmail.com>
2024-01-21 18:15:21 -0300

    Home page (beta version)

--- FrontApp/src/app/components/home-page/home-page.component.ts
+++ FrontApp/src/app/components/home-page/home-page.component.ts
@@ -1,17 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FontAwesomeModule
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
-  image = "assets/home-page/home-back-ground.jpg"
+
+  // IMAGES //  
+  first_top_image = "assets/home-page/home-back-ground-top.jpg"
+  second_bottom_image = "assets/home-page/home-back-ground-bottom.jpg"
+
+  // ICONS //
+  left_arrow_icon = faUpRightFromSquare
+
 }
