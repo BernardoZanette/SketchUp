cc794f56265add859b9ee5f6cba075256eb62bc9
BernardoZanette <bernardo7206@gmail.com>
2024-01-24 16:08:54 -0300

    Replanning the project and then changing the home-page

--- FrontApp/src/app/components/Partials/navbar/navbar.component.ts
+++ FrontApp/src/app/components/Partials/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    FontAwesomeModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  search_icon = faSearch
 
 }
