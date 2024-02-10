2ec52ab59afe6519a63179dc14f51a5d8cfe09db
BernardoZanette <bernardo7206@gmail.com>
2024-02-09 22:36:30 -0300

    Preparing Api authentication code

--- API/routes/api.php
+++ API/routes/api.php
@@ -1,16 +1,13 @@
 <?php
 
-use App\Http\Controllers\API\SketchesController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\SketchesController;
 
 Route::apiResource('/users', UserController::class);
-// Route::delete('users/{id}', [UserController::class, 'destroy']);
-// Route::patch('users/{id}', [UserController::class, 'update']);
-// Route::get('/users/{id}', [UserController::class, 'show']);
-// Route::get('/users', [UserController::class, 'index']);
-// Route::post('/users', [UserController::class, 'store']);
 Route::apiResource('/sketches', SketchesController::class);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/', function () {
     return response()->json([
